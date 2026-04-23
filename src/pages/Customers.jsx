ce21ea41595228e6ee0f17165aac2de9e1a980b3
ramadhani24si-framework <ramadhani24si@mahasiswa.pcr.ac.id>
2026-04-23 15:52:32 +0700

    pertemuan6

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,11 @@
+import { FaShoppingCart, FaTruck, FaBan, FaDollarSign, FaArrowUp, FaArrowDown, FaStar, FaClock } from "react-icons/fa";
+import PageHeader from "../components/PageHeader";
+
+export default function Customers() {
+  return (
+    <div className="space-y-6">
+      <PageHeader />
+      <p> INI HALAMAN Customers</p>
+    </div>
+  );
+}
